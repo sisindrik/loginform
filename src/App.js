0d15e8ddf9ecfc56d93a81548ca1e353b2624cd4
sisindrik <sisindrik@lyrostech.com>
2024-03-10 19:07:46 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginStatus, setLoginStatus] = useState('');
+
+  const UN = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const UP = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+   
+    const EUsername = 'john';
+    const EPassword = 'pass1234'; 
+
+    if (username === EUsername && password === EPassword) {
+      setLoginStatus('Access Granted');
+    } else {
+      setLoginStatus('Access Denied');
+    }
+  };
+
+  const loginStatusStyle = {
+    color: loginStatus === 'Access Granted' ? 'green' : 'red',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <center style={{backgroundColor: "lightblue", height:"100vh"}}>
+      <div className="App">
+      <h1>Login</h1>
+      <div>
+        <label For="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={UN  }
+        />
+      </div>
+      <div>
+        <label For="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={UP}
+        />
+      </div>
+      <button onClick={handleLogin}>Login</button>
+      <p style={loginStatusStyle}>{loginStatus}</p>
     </div>
+    </center>
   );
 }
 
